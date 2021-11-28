9667a558ff25288e284016331d5deca7b342acbd
Abhishek Yadav <36486799+Bitu07@users.noreply.github.com>
2021-11-29 00:46:00 +0530

    Delete Train Final Project/Train Final Project directory

--- Train Final Project/Train Final Project/src/main/java/com/torryharris/trainultimate/repository/TrainRepo.java
+++
@@ -1,8 +0,0 @@
-package com.torryharris.trainultimate.repository;
-
-import com.torryharris.trainultimate.model.Train;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface TrainRepo extends CrudRepository<Train,Integer> {
-}
