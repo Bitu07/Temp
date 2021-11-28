abb0ec29935d559d22987ee9c4013af5aba8bd9b
Abhishek Yadav <36486799+Bitu07@users.noreply.github.com>
2021-11-28 19:49:22 +0530

    Delete TrainReservation - 2/TrainReservation directory

--- TrainReservation - 2/TrainReservation/src/main/java/com/abhi/TrainReservation/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.abhi.TrainReservation.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.abhi.TrainReservation.model.User;
-
-public interface UserRepository extends CrudRepository<User, Integer>{
-	public User findById(int id);
-}
